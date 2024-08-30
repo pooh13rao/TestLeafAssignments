bba4af61004c2b38eca01cef06e4f5306c1851d4
pooh13rao <harshree117@gmail.com>
2024-08-30 12:48:39 +0530

    Update CreateAccount.java

--- week2/CreateAccount.java
+++ week2/CreateAccount.java
@@ -7,21 +7,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CreateAccount {
 
 	public static void main(String[] args) throws InterruptedException {
+		// Launch browser
 		ChromeDriver driver = new ChromeDriver();
+		// maximize web window
 		driver.manage().window().maximize();
+		// load URL
 		driver.get("http://leaftaps.com/opentaps");
+		// find Username element
 		WebElement UserName = driver.findElement(By.id("username"));
+		// enter value
 		UserName.sendKeys("demosalesmanager");
+		// find password element
 		WebElement password = driver.findElement(By.id("password"));
+		// enter value
 		password.sendKeys("crmsfa");
 		driver.findElement(By.className("decorativeSubmit")).click();
-
+		// get the title of the page
 		String title = driver.getTitle();
 		System.out.println("The Title of the page : " + title);
-
+		// Click CRM/SFA
 		driver.findElement(By.linkText("CRM/SFA")).click();
 		driver.findElement(By.linkText("Accounts")).click();
 		driver.findElement(By.linkText("Create Account")).click();
+		//Find the below elements and enter vvalues
 		driver.findElement(By.id("accountName")).sendKeys("Pooja Training");
 		driver.findElement(By.name("description")).sendKeys("Selenium Automation Tester");
 		driver.findElement(By.id("numberEmployees")).sendKeys("7");
