66d27bdc91ade49abe8981cf348ac7d229f1f745
pooh13rao <harshree117@gmail.com>
2024-09-06 20:36:30 +0530

    Add files via upload

---
+++ week3/Strings/reverseOdd.java
@@ -0,0 +1,29 @@
+package assignments.week3.Strings;
+
+import java.util.Arrays;
+
+public class reverseOdd {
+
+	public static void main(String[] args) {
+		String test = "I am a software tester";
+		// Split the string into words
+		String[] input = test.split(" ");
+		System.out.println("input is " + Arrays.toString(input));
+
+		// Traverse through the words
+		for (int i = 0; i < input.length; i++) {
+			if (i % 2 != 0) {
+				// Reverse the word at odd index using String Builder as this has pre defined
+				// method reverse()
+				StringBuilder newString = new StringBuilder(input[i]);
+			//	System.out.println(" before reverse  " + input[i]);
+
+				input[i] = newString.reverse().toString();
+			//	System.out.println(" after reverse  " + input[i]);
+
+			}
+			System.out.print(" " + input[i]);
+		}
+	}
+
+}
---
+++ week3/Strings/upperCase.java
@@ -0,0 +1,25 @@
+package assignments.week3.Strings;
+
+public class upperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "changeme";
+
+		// Convert the given String to a character array.
+		char[] arr = text.toCharArray();
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (i % 2 != 0) {
+				// change the character to uppercase if the index is odd
+				arr[i] = Character.toUpperCase(arr[i]);
+				//System.out.println(arr[i]);
+			}
+		}
+		//character to String convert
+		String newString = new String(arr);
+		System.out.println("The changed String is "+newString);
+
+	}
+
+}
